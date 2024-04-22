48b9babf1df1b24af05cefe0a7ff7443a690ddaa
Rodrigo <rod_novaes@hotmail.com>
2024-04-22 12:31:33 +1200

    Add a header component

---
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { Header } from '../../../../payload/payload-types';
+import { Gutter } from '../../Gutter';
+import { HeaderNav } from '../Nav';
+import { noHeaderFooterUrls } from '../../../constants';
+import { usePathname } from 'next/navigation';
+import MobileNav from '../MobileNav';
+import classes from './index.module.scss';
+
+const HeaderComponent = ({ header }: { header: Header }) => {
+  const pathname = usePathname();
+
+  return (
+    <nav
+      className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide]
+        .filter(Boolean)
+        .join(' ')}
+    >
+      <Gutter className={classes.wrap}>
+        <Link href="/">
+          <Image
+            className={classes.logo}
+            src="/logo-black.svg"
+            alt="logo"
+            width={170}
+            height={50}
+          />
+        </Link>
+        <HeaderNav header={header} />
+      </Gutter>
+    </nav>
+  );
+};
+
+export default HeaderComponent;
